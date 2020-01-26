810d8cab3d475423bfd4489978b0e738fdb7e247
Neil Solomon <neilsolomon89@gmail.com>
2020-01-26 18:07:48 -0500

    Updates

---
+++ precache-manifest.87329784681a2d82d31e32ac47d3ad18.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ab5083e1cd5bc7a82e1572017c45ce0",
+    "url": "/horoscopeApp/index.html"
+  },
+  {
+    "revision": "56bd5c6806391d09b337",
+    "url": "/horoscopeApp/static/css/main.2c8e1964.chunk.css"
+  },
+  {
+    "revision": "d235eca1f1a0ee268258",
+    "url": "/horoscopeApp/static/js/2.054c4bed.chunk.js"
+  },
+  {
+    "revision": "53adde51d1fa21b871fe9b19662ee999",
+    "url": "/horoscopeApp/static/js/2.054c4bed.chunk.js.LICENSE"
+  },
+  {
+    "revision": "56bd5c6806391d09b337",
+    "url": "/horoscopeApp/static/js/main.943e8110.chunk.js"
+  },
+  {
+    "revision": "7e25b1ca54c47a67d80a",
+    "url": "/horoscopeApp/static/js/runtime-main.a654153f.js"
+  }
+]);
